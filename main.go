34874fd82095cec698097f596f90f0cadfcce2f2
John E. Rollinson <j.e.rollinson@gmail.com>
2019-01-20 12:49:49 -0500

    Update parsing

--- main.go
+++ main.go
@@ -2,9 +2,19 @@ package main
 
 import (
 	"bytes"
+	"encoding/json"
 	"log"
 	"net"
 )
+
+type Message struct {
+	Time 		string	`json:"time"`
+	Model 		string	`json:"model"`
+	Id 			int		`json:"id"`
+	Channel 	int		`json:"channel"`
+	Temperature	float64	`json:"temperature_C"`
+	Humidity 	int		`json:"humidity"`
+}
 
 func main() {
 	server, err := net.ListenUDP("udp", &net.UDPAddr{Port: 4242})
@@ -13,30 +23,39 @@ func main() {
 	}
 	defer server.Close()
 
-	msg := make([]byte, 255)
-	prev_msg := make([]byte, 255)
+	dat := make([]byte, 255)
+	prev_dat := make([]byte, 255)
+
+	msg := Message{}
 	for {
-		n, _, err := server.ReadFrom(msg)
+		n, _, err := server.ReadFrom(dat)
 		if err != nil {
 			log.Fatal(err)
 		}
-		if bytes.Equal(msg, prev_msg) {
+		if bytes.Equal(dat, prev_dat) {
 			continue
 		}
-		copy(prev_msg, msg)
+		copy(prev_dat, dat)
 		i := 0
 		for ; i < n; i++ {
-			if msg[i] == '{' {
+			if dat[i] == '{' {
 				break
 			}
 		}
 
 		if i == n {
-			log.Print(string(msg[:n]))
 			log.Print("invalid message received")
 			continue
 		}
 
-		log.Print(string(msg[i-1 : n]))
+		err = json.Unmarshal(dat[i:n], &msg)
+		if err != nil {
+			log.Print(err)
+			continue
+		}
+
+		msg.Temperature = (9.0*msg.Temperature/5.0) + 32.0
+
+		log.Printf("Channel %d: %10.1f F | %4d%%", msg.Channel, msg.Temperature, msg.Humidity)
 	}
 }
